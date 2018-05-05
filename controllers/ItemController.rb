dfcb494ad6436c8d679aa518f7ebe431cb21f6a7
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-05 15:45:54 -0500

    delete route

--- controllers/ItemController.rb
+++ controllers/ItemController.rb
@@ -37,4 +37,13 @@ class ItemController < ApplicationController
 	    # @item.to_json
 	    redirect '/items'
 	end
+
+	delete '/:id' do
+		# there are many ways to do this find statement, this is just one
+		# remember you can play around with ActiveRecord by adding binding.pry 
+		# and trying stuff out
+		@item = Item.find params[:id]
+		@item.destroy
+		redirect '/items'
+	end
 end
