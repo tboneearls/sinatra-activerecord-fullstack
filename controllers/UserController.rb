a699eca703d288e4a5e15f1f18fdcd30ff26e330
Tyler Earls <tyler.a.earls@gmail.com>
2018-05-05 23:50:27 -0500

    login and register works

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -13,11 +13,31 @@ class UserController < ApplicationController
 	end
 
 	post '/login' do
-		params.to_json
+		@user = User.find_by(username: params[:username])
+
+		if @user && @user.password == params[:password]
+			session[:username] = @user.username
+			session[:logged_in] = true
+			session[:message] = "Logged in as #{@user.username}"
+			redirect '/items'
+		else
+			session[:message] = "Invalid username or password."
+			redirect '/user/login'
+		end
 	end
 
 	post '/register' do
-		params.to_json
+		# instantiate user in database
+		# update user attributes using input from register form
+		@user = User.new
+		@user.username = params[:username]
+		@user.password = params[:password]
+		@user.save
+		# save info to session
+		session[:logged_in] = true
+		session[:username] = @user.username
+		session[:message] = "Thank you for registering (as #{@user.username}).  Enjoy the site!"
+		redirect '/items'	
 	end
 
 end
